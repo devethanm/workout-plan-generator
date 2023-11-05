9c583862486f6aacd81804a8bf0d3af395bdf9e6
Ethan Maestas <ethanmdev@gmail.com>
2023-11-04 18:43:50 -0600

    feat: first working api responses, hardcoded values, no db

--- amplify/backend/function/workoutplangeneratora79fa11a/src/index.js
+++ amplify/backend/function/workoutplangeneratora79fa11a/src/index.js
@@ -6,9 +6,18 @@
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
-    const workoutType = event.pathParameteres.workoutType;
-    console.log(workoutType);
-    // const workout = {'workoutId': 1}
+    const workoutType = String(event.pathParameters.workoutType);
+    let workout;
+
+    // hardcoded responses for now (no db)
+    if (workoutType == "arms") 
+        workout = {'workoutId': 1, type: workoutType, 'length': "20 minutes"};
+    else if (workoutType == "legs") 
+        workout = {'workoutId': 2, type: workoutType, 'length': "40 minutes"};
+    else if (workoutType == "back") 
+        workout = {'workoutId': 3, type: workoutType, 'length': "1 hour"};
+    else 
+        workout = {'workoutId': 1, type: workoutType, 'length': "1 hour"};
 
     return {
         statusCode: 200,
@@ -17,6 +26,8 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-        body: JSON.stringify(workoutType),
+        //body: JSON.stringify("hello"),
+        body: JSON.stringify(workout),
     };
 };
+
